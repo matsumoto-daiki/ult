ac7f973aa2595ff3a812c9cb4677fe952f16da65
matsumoto-daiki <rusty.chatnoir@gmail.com>
2018-02-18 19:53:00 +0900

    環境構築用ファイル追加:

---
+++ bin/init.bat
@@ -0,0 +1,14 @@
+@echo off
+cd /d %~dp0
+for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
+if NOT "%LEVEL%"=="High" (
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Start-Process %~f0 -Verb runas"
+exit
+)
+@rem "chocolatey"をインストールする
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+@rem 各種パッケージをインストールする
+cinst php composer vim git
+
+echo インストールが完了しました！
